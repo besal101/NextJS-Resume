fe8e134d29fdfdd8847011b390b7af2c80ec7ccb
besal101 <bishal.lifesmile@gmail.com>
2022-03-12 11:02:21 +0400

    Finalised

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
   env: {
     SERVICEID: 'YOUR SERVICE ID HERE',
     TEMPLATEID: 'YOUR TEMPLATE ID HERE',
     USERID: 'YOUR USER ID HERE',
   },
-=======
->>>>>>> 2b90f52b14653c8abf0d11df24c77ff5085c0f79
 };
 
 module.exports = nextConfig;
