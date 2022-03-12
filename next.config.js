2b90f52b14653c8abf0d11df24c77ff5085c0f79
Bishal Khatri <83866055+besal101@users.noreply.github.com>
2022-03-12 10:25:25 +0400

    Removed the Configs :P

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    SERVICEID: 'service_ltwhh4u',
-    TEMPLATEID: 'template_8prbjpn',
-    USERID: 'DEM0Rp56kWGfbGpyE',
-  },
 };
 
 module.exports = nextConfig;
